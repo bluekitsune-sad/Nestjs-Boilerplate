68a97db395a23b17183e2d30ebbbfce188a76b5e
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2025-02-25 19:43:15 +0500

    inProcess

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Users } from './schemas/Users.schema';
 import { Model } from 'mongoose';
 import { CreateUsersDto } from './dto/CreateUsers.dto';
+import { UpdateUsersDto } from './dto/UpdateUsers.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,4 +16,19 @@ export class UsersService {
         return newUser.save();
     }
 
+    getUsers() {
+        return this.userModel.find();
+    }
+
+    getUserById(id: string) {
+        return this.userModel.findById(id);
+    }
+
+    updateUser(id: string, updateUserDto: UpdateUsersDto) {
+        return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+    }
+
+    deleteUser(id: string) {
+        return this.userModel.findByIdAndDelete(id);
+    }
 }
