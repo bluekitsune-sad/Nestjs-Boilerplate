6dc783d1ef17a82b250d73d5a5c8b7b9077c3bc1
BlueKitsune狐 <116144344+bluekitsune-sad@users.noreply.github.com>
2025-02-25 19:42:48 +0500

    inProcess

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,70 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UsePipes,
+  Get,
+  Param,
+  HttpException,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUsersDto } from './dto/CreateUsers.dto';
+import mongoose from 'mongoose';
+import { UpdateUsersDto } from './dto/UpdateUsers.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
 
-    constructor(private usersService: UsersService) {}
+  @Post()
+  @UsePipes(new ValidationPipe)
+  createUser(@Body() createUserDto: CreateUsersDto) {
+    return this.usersService.createUser(createUserDto);
+  }
 
-    @Post()
-    // @UsePipes(ValidationPipe)
-    createUser(@Body() createUserDto: CreateUsersDto) {
-        return this.usersService.createUser(createUserDto);
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new HttpException('Invalid ID', 400);
     }
+    const user = await this.usersService.getUserById(id);
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+    return user;
+  }
+
+  @Put(':id')
+  @UsePipes(new ValidationPipe)
+  async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUsersDto) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new HttpException('Invalid ID', 400);
+      }
+    const user = await this.usersService.updateUser(id, updateUserDto);
+    if (!user) {
+        throw new HttpException('User not found', 404);
+    }
+    return user;
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new HttpException('Invalid ID', 400);
+    }
+    const user = await this.usersService.deleteUser(id);
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+    return user;
+  }
 }
+
